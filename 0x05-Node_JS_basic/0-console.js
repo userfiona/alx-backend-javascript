cb70f80aab01b66dd321b73ce1df35732dc7aff7
userfiona <fionaramaesela.com>
2024-02-27 12:50:41 +0000

    it's a runtime environment that's used to run JavaScript outside the browser.

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,5 @@
-/**
- * Prints the given message to STDOUT.
- *
- * @param {msg} str - The message to be printed.
- */
-function displayMessage(msg) {
-  console.log(msg);
+function displayMessage(str) {
+  console.log(str);
 }
 
 module.exports = displayMessage;
